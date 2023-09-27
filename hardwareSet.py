b5bfdfee28ada6b17c7d12f21647bc889fa69239
Chinmay Krishna <chinmay.krishna@utexas.edu>
2023-09-27 11:54:45 -0500

    updated functionality when checking out more than available

--- hardwareSet.py
+++ hardwareSet.py
@@ -2,13 +2,13 @@ class hardwareSet:
     def __init__(self):
         self.__capacity = 0
         self.__availability = 0
-        self.__inUse = 0
     def get_availability(self):
         return self.__availability
     def get_capacity(self):
         return self.__capacity
     def check_out(self, qty):
         if(self.__availability < qty):
+            self.__availability = 0
             return -1
         self.__availability -= qty
         return 0
