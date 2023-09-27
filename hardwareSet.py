b6d2d201a02493f4f4ce34310e8274a51bd85f2c
Chinmay Krishna <chinmay.krishna@utexas.edu>
2023-09-27 08:49:04 -0500

    added hardwareSet.py to keep track of availablity, capacity, inUse, and allow users to check in and check out

---
+++ hardwareSet.py
@@ -0,0 +1,20 @@
+class hardwareSet:
+    def __init__(self):
+        self.__capacity = 0
+        self.__availability = 0
+        self.__inUse = 0
+    def get_availability(self):
+        return self.__availability
+    def get_capacity(self):
+        return self.__capacity
+    def check_out(self, qty):
+        if(self.__availability < qty):
+            return -1
+        self.__availability -= qty
+        return 0
+    def check_in(self,qty):
+        self.__availability += qty
+    def initialize_capacity(self, qty):
+        self.__capacity = qty
+        self.__availability = qty
+
