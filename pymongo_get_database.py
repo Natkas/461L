e2bed38e998b24906e00dea85512be249120fc9e
Chinmay Krishna <chinmay.krishna@utexas.edu>
2023-10-11 08:18:25 -0500

    Added CRUD operations for database

--- pymongo_get_database.py
+++ pymongo_get_database.py
@@ -2,9 +2,7 @@ from pymongo import MongoClient
 
 def get_database():
     CONNECTION_STRING = "mongodb+srv://ckrishna:pass@cluster0.b99hdoa.mongodb.net/"
-
     client = MongoClient(CONNECTION_STRING)
-
     db = client['461L']
 
     if 'users' not in db.list_collection_names():
@@ -37,5 +35,55 @@ def add_project(name, description, amount_available, amount_checked_out, owner, 
     }
     projects_collection.insert_one(project_data)
 
-if __name__ == "__main__":
+def get_users():
+    db = get_database()
+    users_collection = db['users']
+    return list(users_collection.find())
+
+def get_projects():
+    db = get_database()
+    projects_collection = db['projects']
+    return list(projects_collection.find())
+
+def get_user(username):
+    db = get_database()
+    users_collection = db['users']
+    return users_collection.find_one({'username': username})
+
+def get_project(name):
+    db = get_database()
+    projects_collection = db['projects']
+    return projects_collection.find_one({'name': name})
+
+def update_user_password(username, new_password):
+    db = get_database()
+    users_collection = db['users']
+    users_collection.update_one({'username': username}, {'$set': {'password': new_password}})
+
+def update_project_details(name, new_description, new_amount_available, new_amount_checked_out, new_members):
+    db = get_database()
+    projects_collection = db['projects']
+    projects_collection.update_one(
+        {'name': name},
+        {
+            '$set': {
+                'description': new_description,
+                'amountAvailable': new_amount_available,
+                'amountCheckedOut': new_amount_checked_out,
+                'members': new_members
+            }
+        }
+    )
+
+def delete_user(username):
+    db = get_database()
+    users_collection = db['users']
+    users_collection.delete_one({'username': username})
+
+def delete_project(name):
+    db = get_database()
+    projects_collection = db['projects']
+    projects_collection.delete_one({'name': name})
+
+if __name__ == "__main":
     dbname = get_database()
--- pymongo_test_insert.py
+++ pymongo_test_insert.py
@@ -1,11 +1,55 @@
-from pymongo_get_database import get_database, add_user, add_project
+from pymongo_get_database import get_database, add_user, add_project, get_users, get_projects, update_user_password, update_project_details, delete_user, delete_project
 
 # Get the database
 dbname = get_database()
 
-# Add a user
+# Add users and projects
 add_user("user1", "password123")
 add_user("user2", "password234")
 
-# Add a project
 add_project("Project 1", "Description of Project 1", 1000, 0, "user1", ["user1", "user2"])
+
+# Get and display users
+print("Users:")
+users = get_users()
+for user in users:
+    print(f"Username: {user['username']}, Password: {user['password']}")
+
+# Get and display projects
+print("\nProjects:")
+projects = get_projects()
+for project in projects:
+    print(f"Name: {project['name']}")
+    print(f"Description: {project['description']}")
+    print(f"Amount Available: {project['amountAvailable']}")
+    print(f"Amount Checked Out: {project['amountCheckedOut']}")
+    print(f"Owner: {project['owner']}")
+    print(f"Members: {project['members']}")
+
+# Update a user's password
+update_user_password("user1", "newpassword123")
+
+# Update a project's details
+add_user("user3", "password360")
+update_project_details("Project 1", "Updated Description", 2000, 500, ["user1", "user2", "user3"])
+
+# Display updated user and project information
+print("\nUpdated User and Project Information:")
+updated_users = get_users()
+for user in updated_users:
+    print(f"Username: {user['username']}, Password: {user['password']}")
+
+updated_projects = get_projects()
+for project in updated_projects:
+    print(f"Name: {project['name']}")
+    print(f"Description: {project['description']}")
+    print(f"Amount Available: {project['amountAvailable']}")
+    print(f"Amount Checked Out: {project['amountCheckedOut']}")
+    print(f"Owner: {project['owner']}")
+    print(f"Members: {project['members']}")
+
+# Delete
+delete_user("user1")
+delete_user("user2")
+delete_user("user3")
+delete_project("Project 1")
